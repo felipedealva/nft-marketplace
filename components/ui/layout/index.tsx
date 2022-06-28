28e7f38c24eb341c426c7d7cbaedd37c61af1a7d
Felipe de Alva <felipe.dealva@qubit.com>
2022-06-28 16:33:44 +0200

    Layout UI component

--- components/ui/layout/index.tsx
+++ components/ui/layout/index.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent } from "react";
 import Navbar from "../navbar";
 
-const BaseLayout: FunctionComponent = ({ children }) => {
+const BaseLayout: FunctionComponent<any> = ({ children }) => {
   return (
     <>
       <Navbar />
-      <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
-        <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+      <div className="py-16 bg-gray-100 overflow-hidden min-h-screen">
+        <div className="max-w-backba mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
           {children}
         </div>
       </div>
