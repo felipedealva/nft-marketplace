9531186874522a3c8efc2fc7df192a9042e50790
Felipe de Alva <felipe.dealva@qubit.com>
2022-06-28 16:32:05 +0200

    Types

--- types/nft.ts
+++ types/nft.ts
@@ -1,13 +1,37 @@
-export type Trait = "attack" | "health" | "speed"
+export type Trait = "attack" | "health" | "speed";
 
 export type NftAttribute = {
   trait_type: Trait;
   value: string;
-}
+};
 
 export type NftMeta = {
   name: string;
   description: string;
   image: string;
-  attributes: NftAttribute[]
+  attributes: NftAttribute[];
+};
+
+export type NftCore = {
+  tokenId: number;
+  price: number;
+  creator: string;
+  isListed: boolean;
+};
+
+export type Nft = {
+  meta: NftMeta;
+} & NftCore;
+
+export type FileReq = {
+  bytes: Uint8Array;
+  contentType: string;
+  fileName: string;
+};
+
+export type PinataResponse = {
+  IpfsHash: string;
+  PinSize: number;
+  Timestamp: string;
+  isDuplicate: boolean;
 }
