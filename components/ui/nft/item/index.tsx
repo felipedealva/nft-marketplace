9921fa5ec51669f19a9634d34698e2c7ec3fd736
Felipe de Alva <felipe.dealva@qubit.com>
2022-06-28 16:32:49 +0200

    Nft item UI component

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -1,18 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
+import { Nft } from "@_types/nft";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
-import { NftMeta } from "../../../types/nft";
 
 type NftItemProps = {
-  item: NftMeta;
+  item: Nft;
+  buyNft: (tokenId: number, value: number) => Promise<void>;
 };
 
-const NftItem: FunctionComponent<NftItemProps> = ({ item }) => {
+const NftItem: FunctionComponent<NftItemProps> = ({ item, buyNft }) => {
+  const router = useRouter();
   return (
     <>
       <div className="flex-shrink-0">
         <img
           className={`h-full w-full object-cover`}
-          src={item.image}
+          src={item.meta.image}
           alt="New NFT"
         />
       </div>
@@ -20,9 +24,11 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item }) => {
         <div className="flex-1">
           <p className="text-sm font-medium text-indigo-600">Creatures NFT</p>
           <div className="block mt-2">
-            <p className="text-xl font-semibold text-gray-900">{item.name}</p>
+            <p className="text-xl font-semibold text-gray-900">
+              {item.meta.name}
+            </p>
             <p className="mt-3 mb-3 text-base text-gray-500">
-              {item.description}
+              {item.meta.description}
             </p>
           </div>
         </div>
@@ -34,7 +40,7 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item }) => {
               </dt>
               <dd className="order-1 text-xl font-extrabold text-indigo-600">
                 <div className="flex justify-center items-center">
-                  100
+                  {item.price}
                   <img
                     className="h-6"
                     alt="ether icon"
@@ -43,7 +49,7 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item }) => {
                 </div>
               </dd>
             </div>
-            {item.attributes.map((attribute) => (
+            {item.meta.attributes.map((attribute) => (
               <div
                 key={attribute.trait_type}
                 className="flex flex-col px-4 pt-4"
@@ -62,15 +68,24 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item }) => {
           <button
             type="button"
             className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed mr-2 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={() => {
+              buyNft(item.tokenId, item.price);
+            }}
           >
             Buy
           </button>
-          <button
-            type="button"
-            className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          <Link
+            href={`/nft/${item.tokenId}`}
+            onClick={() => router.push(`/nft/${item.tokenId}`)}
           >
-            Preview
-          </button>
+            <button
+              onClick={() => {}}
+              type="button"
+              className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Preview
+            </button>
+          </Link>
         </div>
       </div>
     </>
