2e57991e7a0a9f96ccd7051fc26fbb0987b6c218
Felipe de Alva <felipe.dealva@qubit.com>
2022-06-28 17:31:33 +0200

    Style fixes

--- components/ui/layout/index.tsx
+++ components/ui/layout/index.tsx
@@ -6,7 +6,7 @@ const BaseLayout: FunctionComponent<any> = ({ children }) => {
     <>
       <Navbar />
       <div className="py-16 bg-gray-100 overflow-hidden min-h-screen">
-        <div className="max-w-backba mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
           {children}
         </div>
       </div>
