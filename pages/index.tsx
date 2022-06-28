8763942a22c2f41c0c3dbd0284adeac1d281ac4e
Felipe de Alva <felipe.dealva@qubit.com>
2022-06-28 16:36:52 +0200

    Marketplace page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,15 @@
 
 import type { NextPage } from "next";
 import { BaseLayout, NftList } from "@ui";
-import nfts from "../content/meta.json";
-import { NftMeta } from "@_types/nft";
-import { useWeb3 } from "@providers/web3";
+import { useNetwork } from "@hooks/web3";
+import { ExclamationIcon } from "@heroicons/react/solid";
 
 const Home: NextPage = () => {
-  const { ethereum, provider } = useWeb3();
-
-  const getAccounts = async () => {
-    const accounts = await provider!.listAccounts();
-    console.log(accounts[0]);
-  };
-
-  if (provider) {
-    getAccounts();
-  }
+  const { network } = useNetwork();
 
   return (
     <BaseLayout>
-      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div className="absolute inset-0">
-          <div className="bg-white h-1/3 sm:h-2/3" />
-        </div>
+      <div className="relative bg-gray-100 pb-20 px-4 sm:px-6 lg:pb-28 lg:px-8">
         <div className="relative">
           <div className="text-center">
             <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
@@ -33,7 +20,32 @@ const Home: NextPage = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList nfts={nfts as NftMeta[]} />
+          {network.isConnectedToNetwork ? (
+            <NftList />
+          ) : (
+            <div className="rounded-md bg-yellow-50 p-4 mt-10">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <ExclamationIcon
+                    className="h-5 w-5 text-yellow-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="ml-3">
+                  <h3 className="text-sm font-medium text-yellow-800">
+                    Attention needed
+                  </h3>
+                  <div className="mt-2 text-sm text-yellow-700">
+                    <p>
+                      {network.isLoading
+                        ? "Loading..."
+                        : `Connect to ${network.targetNetwork}`}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </BaseLayout>
